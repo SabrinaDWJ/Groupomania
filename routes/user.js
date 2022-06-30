69ba93a722bf487b4c7c47720eb4c0ad5f652a6c
SabrinaDWJ <92731479+SabrinaDWJ@users.noreply.github.com>
2022-06-30 09:05:26 +0200

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const userCtrl = require("../controllers/user");
+
+router.get("/", userCtrl.getAllUsers);
+router.get("/:id", userCtrl.getOneUser);
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
+router.delete("/delete/:id", userCtrl.deleteUser);
+
+module.exports = router;
